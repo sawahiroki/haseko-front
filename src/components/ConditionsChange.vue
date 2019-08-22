edc07ba8e3cccdcf03408c37781313c3bd9de1e3
澤 弘樹 <sawahiroki@LAB-N1547.local>
2019-08-22 18:35:08 +0900

    条件を変更するUIの作成

--- src/components/ConditionsChange.vue
+++ src/components/ConditionsChange.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="conditionsChange">
-    <h1>条件変更</h1>
+    <h2>条件変更</h2>
+    <b-container fluid>
+    <b-row v-for="(tag, index) of tags" :key="index">
+    <b-col cols="2"><h5>{{ tag.caption }}</h5></b-col>
+    <b-col cols="4"><b-form-input id="index" v-model="tag.value" type="range" min="1" max="5"></b-form-input></b-col>
+    <b-col cols="2"><h5>Value: {{ tag.value }}</h5></b-col>
+    </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tags: [
+        { caption: '角部屋', value: 3 },
+        { caption: '広さ', value: 3 },
+        { caption: '階立て', value: 3 },
+        { caption: '立地', value: 3 },
+        { caption: '駅地下', value: 3 },
+        { caption: 'キッチン', value: 3 }
+      ]
+    }
+  }
+}
+</script>
