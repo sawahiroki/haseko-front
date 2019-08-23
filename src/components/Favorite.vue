0adb6fd6fc72fd71724fd5b3f5783ec9447ff82c
澤 弘樹 <sawahiroki@LAB-N1547.local>
2019-08-23 10:35:40 +0900

    お気に入り一覧の作成

--- src/components/Favorite.vue
+++ src/components/Favorite.vue
@@ -1,5 +1,168 @@
 <template>
   <div class="favorite">
-    <h1>お気に入り</h1>
+    <h2>物件一覧</h2>
+    <br>
+    <!-- <b-button v-b-modal.modal-1>条件を絞り込む</b-button>
+    <b-modal size="xl" id="modal-1" title="条件を絞り込む" title-center >
+      <Conditions />
+    </b-modal> -->
+    <div id="properties-list" v-for="id of favoriteIdListPerPage" :key="id">
+        <PropertyView :item="items[id]" />
+    </div>
+
+    <br>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="favoriteItemNum"
+      :per-page="perPage"
+      aria-controls="properties-list"
+      align="center"
+    ></b-pagination>
   </div>
 </template>
+
+<script>
+import PropertyView from './PropertyView'
+import Conditions from './Conditions'
+
+export default {
+  name: 'Properties',
+  components: {
+    PropertyView,
+    Conditions
+  },
+  data () {
+    return {
+      perPage: 2,
+      currentPage: 1,
+      items: [
+        {
+          propertyId: 0,
+          imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlCFA9ONW498v3SFqV7f9dxS55BxS9pkvZJL7YuQbjajNT43jz',
+          propertyType: '中古マンション',
+          title: 'モア・クレスト荒川公園シエール館',
+          price: 4480,
+          planOfHouse: '4LDK',
+          comment: {
+            l1: 'JR常磐線「三河島駅」徒歩7分'
+          },
+          point: [
+            '１４階建８階部分・南東角部屋',
+            '4LDK・81.95m^2・24時間365・・・'
+          ],
+          occupiedArea: '81.95m^2',
+          builtYearMonth: '1999年9月',
+          favorite: true
+        },
+        {
+          propertyId: 1,
+          imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlCFA9ONW498v3SFqV7f9dxS55BxS9pkvZJL7YuQbjajNT43jz',
+          propertyType: '中古マンション',
+          title: 'モア・クレスト荒川公園シエール館',
+          price: 4480,
+          planOfHouse: '4LDK',
+          comment: {
+            l1: 'JR常磐線「三河島駅」徒歩7分'
+          },
+          point: [
+            '１４階建８階部分・南東角部屋',
+            '4LDK・81.95m^2・24時間365・・・'
+          ],
+          occupiedArea: '81.95m^2',
+          builtYearMonth: '1999年9月',
+          favorite: false
+        },
+        {
+          propertyId: 2,
+          imgSrc: 'https://d1f5hsy4d47upe.cloudfront.net/28/2899126b4f391a47ae6888ab0db8f217_t.jpeg',
+          propertyType: '戸建て',
+          title: '東京都江東区千石3丁目',
+          price: 4480,
+          planOfHouse: '4LDK',
+          comment: [
+            'JR常磐線「三河島駅」徒歩7分'
+          ],
+          point: [
+            '１４階建８階部分・南東角部屋'
+          ],
+          occupiedArea: '81.95m^2',
+          builtYearMonth: '1999年9月',
+          favorite: true
+        },
+        {
+          propertyId: 3,
+          imgSrc: 'https://d1f5hsy4d47upe.cloudfront.net/28/2899126b4f391a47ae6888ab0db8f217_t.jpeg',
+          propertyType: '戸建て',
+          title: '東京都江東区千石3丁目',
+          price: 4480,
+          planOfHouse: '4LDK',
+          comment: [
+            'JR常磐線「三河島駅」徒歩7分'
+          ],
+          point: [
+            '１４階建８階部分・南東角部屋'
+          ],
+          occupiedArea: '81.95m^2',
+          builtYearMonth: '1999年9月',
+          favorite: true
+        }
+      ]
+    }
+  },
+  computed: {
+    allItemNum () {
+      return this.items.length
+    },
+    favoriteIdList () {
+      let favoriteIdList = []
+      for (let i = 0; i < this.allItemNum; i++) {
+        if (this.items[i].favorite) {
+          favoriteIdList.push(i)
+        }
+      }
+      return favoriteIdList
+    },
+    favoriteItemNum () {
+      return this.favoriteIdList.length
+    },
+    favoriteItemMaxId () {
+      return this.favoriteIdList[this.favoriteItemNum - 1]
+    },
+    favoriteIdListPerPage () {
+      let favoriteIdListPerPage = []
+      for (var i = 0; i < this.perPage; i++) {
+        if (this.favoriteIdList[(this.currentPage - 1) * this.perPage + i] <= this.favoriteItemMaxId) {
+          favoriteIdListPerPage.push(this.favoriteIdList[(this.currentPage - 1) * this.perPage + i])
+        }
+      }
+      return favoriteIdListPerPage
+    }
+  }
+}
+</script>
+
+<style scoped>
+ol {
+  margin-left: auto;
+  width: 480px;
+  display:flex;
+  justify-content: center;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  width: 100px;
+  height: 50px;
+  margin-left:5px;
+  margin-right:5px;
+  font-size: 16px;
+  background-color: gray;
+  border-radius: 14px;
+  position: relative;
+}
+
+li:hover {
+  opacity: 0.4;
+}
+</style>
